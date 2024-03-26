0e22ef5f6022f4b09e3fc3984db9c83900238b99
Jordan Rowland <46544691+undo-k@users.noreply.github.com>
2024-03-26 12:18:20 -0400

    update source link

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,3 @@
 <template>
-    <span>[<a href="https://github.com/undo-k/smite-one/">source</a>] [made with ❤️️]</span>
+    <span>[<a href="https://github.com/undo-k/smite-app/">source</a>] [made with ❤️️]</span>
 </template>
