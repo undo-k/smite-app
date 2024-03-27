d9b31cd12187a464270dbbf543fe80aa3c255fe6
Jordan Rowland <46544691+undo-k@users.noreply.github.com>
2024-03-27 15:05:06 -0400

    title change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
         head: {
             charset: "UTF-8",
             viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
-            title: "Smite INFO",
+            title: "Smite One",
             // link: [{rel: "stylesheet", href: "./assets/main.css"}]
         },
     },
